1f62f860cd59b51d06e81ea3d1284c50b4030152
baraunab <bbarauna.beatriz@gmail.com>
2026-04-23 22:10:32 -0400

    TODO em AlunoController

--- src/entidade/AlunoController.java
+++ src/entidade/AlunoController.java
@@ -7,6 +7,7 @@ public class AlunoController {
 	private List<Aluno> alunos = new ArrayList<Aluno>();
 	
 	public void importarCandidatos(List<Candidato> candidatos) {
+		//TODO concluir importacao dos dados de candidato em alunos
 		for(Candidato candidato : candidatos) {
 			Aluno aluno = (Aluno) candidato;
 			aluno.getNumeroInscricao();
